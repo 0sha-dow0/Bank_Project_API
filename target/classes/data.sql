a30510c352d41c2c779ae6677fc9310d9f0078ba
0sha-dow0 <shaikhshadab.hossain@gmail.com>
2023-03-26 12:38:49 +0530

    added the target folder

---
+++ target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO BANKENTITY(account_number,custumer_name,type,bank_name,ifsc,pan,amount,active) VALUES (11111,'Hermione','current account','axis bank','UTIB0000009',440739148,1545000.81,FALSE);
+INSERT INTO BANKENTITY(account_number,custumer_name,type,bank_name,ifsc,pan,amount,active) VALUES (22222,'Aloin','savings account','icici bank','ICIC0000002',910934868,7283000.00,TRUE);
+INSERT INTO BANKENTITY(account_number,custumer_name,type,bank_name,ifsc,pan,amount,active) VALUES (33333,'Sena','current account','sbi bank','SBIN0000813',17691981,4210000.74,FALSE);
+INSERT INTO BANKENTITY(account_number,custumer_name,type,bank_name,ifsc,pan,amount,active) VALUES (44444,'Tressa','savings account','hdfc bank','HDFC0000076',566414680,5401000.26,TRUE);
+INSERT INTO BANKENTITY(account_number,custumer_name,type,bank_name,ifsc,pan,amount,active) VALUES (55555,'Muriel','current account','axis bank','UTIB0000009',665723007,3020000.37,FALSE);
+INSERT INTO BANKENTITY(account_number,custumer_name,type,bank_name,ifsc,pan,amount,active) VALUES (66666,'Braden','savings account','sbi bank','SBIN0000813',13442930,3033000.99,FALSE);
+INSERT INTO BANKENTITY(account_number,custumer_name,type,bank_name,ifsc,pan,amount,active) VALUES (77777,'Oralee','current account','axis bank','UTIB0000009',928786082,9394000.98,TRUE);
